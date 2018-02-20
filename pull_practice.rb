983f927abb3bef7fd4c6e52527fed6afb0bc5ad1
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-02-20 12:01:48 -0800

    Uncomment quack sound

--- pull_practice.rb
+++ pull_practice.rb
@@ -3,13 +3,13 @@
 # of code via git. In the interest of time we'll be uncommenting
 # code rather than writing it, but the same principals will apply.
 
-# Task 1: Pulling in changes
-# One partner should uncomment this method, add and
-# commit the changes and push to GitHub. The other
-# partner should run git pull to retrieve the changes.
-# def duck_noise
-#   puts "quack"
-# end
+Task 1: Pulling in changes
+One partner should uncomment this method, add and
+commit the changes and push to GitHub. The other
+partner should run git pull to retrieve the changes.
+def duck_noise
+  puts "quack"
+end
 
 # Task 2: Same as task one, but switch roles
 # def truck_noise
